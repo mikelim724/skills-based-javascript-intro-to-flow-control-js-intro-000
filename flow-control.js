7e573e545f5e04e235f5f25379ceeaca760930e9
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-19 20:37:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,5 +30,11 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+switch(age){
+  case age>=13 && age <=19:
+    console.log("You are a teenager");
+    break;
+  default:
+    console.log("You have an age");
+  }
 }
